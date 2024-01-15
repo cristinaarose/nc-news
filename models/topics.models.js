61ba1bef50c344e6163789dfa01b9644e9918eef
cristinaarose <cristinarosehart@gmail.com>
2024-01-15 17:01:54 +0000

    add tests and solution to GET/api ticket

--- models/topics.models.js
+++ models/topics.models.js
@@ -1,5 +1,6 @@
 const { topicsData } = require("../db/data/test-data");
 const db = require("../db/connection.js");
+const fs = require("fs/promises");
 
 exports.fetchTopicData = () => {
   let query = `SELECT * FROM topics;`;
