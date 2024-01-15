915a1054c530af68d2aec037218815729f8c161d
cristinaarose <cristinarosehart@gmail.com>
2024-01-15 17:04:31 +0000

    remove console.logs

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const {
 } = require("./controllers/topics.controllers.js");
 const app = express();
 
-app.use(express.json());
-
 app.get("/api/topics", getTopics);
 
 app.get("/api", getEndpoints);
