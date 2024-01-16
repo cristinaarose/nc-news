1cb27d69a6397a4d8e295b916fc84c8c4e892b48
cristinaarose <cristinarosehart@gmail.com>
2024-01-16 12:16:51 +0000

    add tests and solutions to GET article by ID

--- app.js
+++ app.js
@@ -3,10 +3,27 @@ const {
   getTopics,
   getEndpoints,
 } = require("./controllers/topics.controllers.js");
+const { getArticleById } = require("./controllers/articles.controllers.js");
 const app = express();
 
 app.get("/api/topics", getTopics);
 
 app.get("/api", getEndpoints);
+
+app.get("/api/articles/:article_id", getArticleById);
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err.msg === "Not found") {
+    res.status(404).send({ msg: err.msg });
+  }
+});
 
 module.exports = app;
